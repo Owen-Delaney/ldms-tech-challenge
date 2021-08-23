fe33a2acd1bb95a8f3526319af750642343a3bdd
Owen-Delaney <owen_delaney@outlook.com>
2021-08-23 21:24:47 +0100

    Add Swagger configuration and empty controller

---
+++ src/main/java/owen/delaney/ldms/tech/challenge/controller/AmortisationController.java
@@ -0,0 +1,8 @@
+package owen.delaney.ldms.tech.challenge.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AmortisationController {
+	
+}
